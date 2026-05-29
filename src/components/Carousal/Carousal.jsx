d63f34aa815c37342c2ae5a89f185444d343d730
gururajmudakannavar17-lang <mudakannavargururaj@gmail.com>
2026-05-29 15:51:15 +0530

    updated code

--- src/components/Carousal/Carousal.jsx
+++ src/components/Carousal/Carousal.jsx
@@ -1,45 +1,33 @@
-// import React, { useEffect } from "react";
-// import styles from "./Carousal.module.css";
-// //@ts-ignore
-// import Swiper from "swiper";
-// import { useSwiper,SwiperSlide  } from 'swiper/react';
-// import "swiper/css";
-// import  CarousalLeftNavigation  from "./CarousalLeftNavigation/CarousalLeftNavigation";
-// import  CarousalRightNavigation  from "./CarousalRightNavigation/CarousalRightNavigation";
- import { Navigation} from 'swiper/modules';
-
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react'
+import { Navigation } from 'swiper/modules';
+import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import styles from './Carousal.module.css';
 import { useEffect } from 'react';
-
-import 'swiper/css'
-// import { CarousalLeftNavigation } from './CarousalLeftNavigation/CarousalLeftNavigation';
-// import { CarousalRightNavigation } from './CarousalRightNavigation/CarousalRightNavigation';
+import 'swiper/css';
 
 const Controls = ({ data }) => {
-   
   const swiper = useSwiper();
   useEffect(() => {
     swiper.slideTo(0);
   }, [data]);
   return <></>;
 };
-const Carousal = ({ data,renderCardComponent }) => {
+
+const Carousal = ({ data = [], renderCardComponent }) => {
   return (
     <div className={styles.Wrapper}>
       <Swiper
         initialSlide={0}
-        modules={{ Navigation }}
+        modules={[Navigation]}
         slidesPerView={"auto"}
         spaceBetween={40}
         allowTouchMove
       >
         <Controls data={data} />
-        {/* <CarousalLeftNavigation/>
-        <CarousalRightNavigation/> */}
-        {data.map((item)=>{
-            <SwiperSlide>{renderCardComponent(item)}</SwiperSlide>
-        })}
+        {data.map((item) => (
+          <SwiperSlide key={item.id}>
+            {renderCardComponent(item)}
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
--- src/components/Sections/Section.jsx
+++ src/components/Sections/Section.jsx
@@ -5,16 +5,18 @@ import styles from "./Section.module.css";
 import Carousal from "../Carousal/Carousal";
 
 const Section = ({ title, data, type }) => {
-  const [carosalToggle, setCarosalToggle] = useState();
+  const [carosalToggle, setCarosalToggle] = useState(false);
+
   const handleToggle = () => {
     setCarosalToggle(!carosalToggle);
   };
+
   return (
     <div>
       <div className={styles.header}>
         <h3>{title}</h3>
         <h4 className={styles.toggleText} onClick={handleToggle}>
-          {carosalToggle ? "Show all" : "Collapse all"}
+          {carosalToggle ? "Collapse all" : "Show all"}
         </h4>
       </div>
       {data.length === 0 ? (
@@ -22,13 +24,18 @@ const Section = ({ title, data, type }) => {
       ) : (
         <div className={styles.cardWrapper}>
           {!carosalToggle ? (
-            <div className={styles.wrapper} >
+            <div className={styles.wrapper}>
               {data.map((item) => (
                 <Card key={item.id} data={item} type={type} />
               ))}
             </div>
           ) : (
-            <Carousal renderCardComponent={(item)=> <Card key={item.id} data={item} type={type} />} />
+            <Carousal
+              data={data}
+              renderCardComponent={(item) => (
+                <Card key={item.id} data={item} type={type} />
+              )}
+            />
           )}
         </div>
       )}
