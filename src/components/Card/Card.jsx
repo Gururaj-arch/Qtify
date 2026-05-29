6d7f50ea14602bb1ff4c7cc31cce9637012db8a6
gururajmudakannavar17-lang <mudakannavargururaj@gmail.com>
2026-05-29 17:03:59 +0530

    updated code

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,21 +7,36 @@ const Card = ({ data, type }) => {
     switch (type) {
       case "album": {
         const { image, follows, title, songs } = data;
-        console.log(songs,"songs")
-
         return (
-          <Tooltip title={`${songs.length} songs`} placement="top" arrow >
+          <Tooltip title={`${songs.length} songs`} placement="top" arrow>
             <div className={Styles.wrapper}>
               <div className={Styles.card}>
-                <img src={image} alt="album"/>
+                <img src={image} alt="album" />
                 <div className={Styles.banner}>
                   <Chip
                     className={Styles.chip}
                     label={`${follows} Follows`}
-                    Follows
                     size="small"
                   />
                 </div>
+              </div>
+              <div className={Styles.titleWrapper}>
+                <p>{title}</p>
+              </div>
+            </div>
+          </Tooltip>
+        );
+      }
+      case "song": {
+        const { image, title, durationInMs } = data;
+        const minutes = Math.floor(durationInMs / 60000);
+        const seconds = Math.floor((durationInMs % 60000) / 1000);
+        const duration = `${minutes}:${seconds.toString().padStart(2, "0")}`;
+        return (
+          <Tooltip title={duration} placement="top" arrow>
+            <div className={Styles.wrapper}>
+              <div className={Styles.card}>
+                <img src={image} alt={title} />
               </div>
               <div className={Styles.titleWrapper}>
                 <p>{title}</p>
