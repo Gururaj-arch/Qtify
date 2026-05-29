9ab1935bf4404604fff01e545934b2b577b0fef4
gururajmudakannavar17-lang <mudakannavargururaj@gmail.com>
2026-05-29 16:00:55 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import Card from "./components/Card/Card";
 import HeroSection from "./components/HeroSection/HeroSection";
-//@ts-ignore
 import NavBar from "./components/NavBar/NavBar";
 import { fetchTopAlbums } from "./components/api/api";
 import Section from "./components/Sections/Section";
 
 function App() {
-  const [topAlbumData, settopAlbumData] = useState([]);
+  const [topAlbumData, setTopAlbumData] = useState([]);
+
   const generateTopAlbumData = async () => {
-    const data = await fetchTopAlbums();
-    console.log(data);
-    settopAlbumData(data);
+    try {
+      const data = await fetchTopAlbums();
+      if (Array.isArray(data)) {
+        setTopAlbumData(data);
+      }
+    } catch (err) {
+      console.error("Failed to fetch albums:", err);
+    }
   };
+
   useEffect(() => {
     generateTopAlbumData();
   }, []);
-  console.log(topAlbumData, "topAlbumData");
+
   return (
     <div className="App">
       <NavBar />
       <HeroSection />
-      <div className="sectionWrapper" >
-      <Section type="album" title="Top Albums" data={topAlbumData} />
+      <div className="sectionWrapper">
+        {topAlbumData.length > 0 && (
+          <Section type="album" title="Top Albums" data={topAlbumData} />
+        )}
       </div>
-      {/* <div className="cardContainer">
-      {topAlbumData.map((item) => {
-        return <Card key={item.id} data={item} type="album" />;
-      })}
-      </div> */}
     </div>
   );
 }
