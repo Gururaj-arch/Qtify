1cd411d13a39aab6958c19c00411eb215386e071
gururajmudakannavar17-lang <mudakannavargururaj@gmail.com>
2026-05-29 16:39:21 +0530

    updated code

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -1,11 +1,10 @@
-import axios from "axios";
-
 export const BACK_ENDPOINT = "https://qtify-backend-labs.crio.do";
 
 export const fetchTopAlbums = async () => {
   try {
-    const res = await axios.get(`${BACK_ENDPOINT}/albums/top`);
-    return res.data;
+    const res = await fetch(`${BACK_ENDPOINT}/albums/top`);
+    const data = await res.json();
+    return data;
   } catch (err) {
     console.error(err);
   }
@@ -13,8 +12,9 @@ export const fetchTopAlbums = async () => {
 
 export const fetchNewAlbums = async () => {
   try {
-    const res = await axios.get(`${BACK_ENDPOINT}/albums/new`);
-    return res.data;
+    const res = await fetch(`${BACK_ENDPOINT}/albums/new`);
+    const data = await res.json();
+    return data;
   } catch (err) {
     console.error(err);
   }
@@ -22,8 +22,9 @@ export const fetchNewAlbums = async () => {
 
 export const fetchSongs = async () => {
   try {
-    const res = await axios.get(`${BACK_ENDPOINT}/songs`);
-    return res.data;
+    const res = await fetch(`${BACK_ENDPOINT}/songs`);
+    const data = await res.json();
+    return data;
   } catch (err) {
     console.error(err);
   }
